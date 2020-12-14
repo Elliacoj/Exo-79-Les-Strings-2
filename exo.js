c92d198e07bf716a8d920c64b476d5c242f0660f
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-14 09:58:15 +0100

    Modif

--- exo.js
+++ exo.js
@@ -1,3 +1,22 @@
 let pomme = "Elle est belle ma pomme !";
 
-console.log(pomme.lastIndexOf('m'));
+document.getElementById("index-pomme").innerHTML = pomme.lastIndexOf('pomme');
+
+document.getElementById("last-index-m").innerHTML = pomme.lastIndexOf("m");
+
+if(pomme.startsWith("Elle")) {
+    console.log(pomme + " Commence par elle");
+}
+
+if(pomme.endsWith("!")) {
+    console.log(pomme + " Se termine par un point d'exclamation");
+}
+
+document.getElementById("pomme").innerHTML = pomme.substring(18, 22);
+
+let array = [pomme.length];
+console.log(array);
+
+for (let x = 0; x < pomme.length; x++) {
+    document.getElementById("one-line").innerHTML += "</br>" + pomme[x];
+}
